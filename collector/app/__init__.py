4a1bef65051205a16d60d13ba373800d9c9b7288
Gray-Rice <155818488+Gray-Rice@users.noreply.github.com>
2025-09-20 16:33:14 +0530

    Move secrets

--- collector/app/__init__.py
+++ collector/app/__init__.py
@@ -2,15 +2,15 @@ from flask import Flask
 from app.models import db
 from app.routes.events import events_bp
 from app.routes.devices import devices_bp
-import yaml
+import os
 
 def create_app(config_path="config.yaml"):
     app = Flask(__name__)
     
     # Load config
-    with open(config_path, "r") as f:
-        cfg = yaml.safe_load(f)
-    app.config["SQLALCHEMY_DATABASE_URI"] = cfg["database"]["uri"]
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
+            "DATABASE_URL", "postgresql://ids_user:ids_pass@localhost:5432/ids_db"
+        )
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     
     db.init_app(app)
